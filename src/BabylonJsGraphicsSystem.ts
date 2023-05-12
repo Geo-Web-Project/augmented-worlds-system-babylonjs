be50da58f7c49a70480bab86d484944a1568da87
Cody Hatfield <cody.hatfield@me.com>
2023-05-12 15:30:56 -0700

    Add simple graphics

--- src/BabylonJsGraphicsSystem.ts
+++ src/BabylonJsGraphicsSystem.ts
@@ -6,15 +6,17 @@ import { Scene } from "@babylonjs/core/scene";
 import { Vector3 } from "@babylonjs/core/Maths/math";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+// import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+// import { Tools } from "@babylonjs/core/Misc/tools";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 
 // Side-effects only imports allowing the standard material to be used as default.
 import "@babylonjs/core/Materials/standardMaterial";
+import "@babylonjs/core/Meshes/Builders/groundBuilder";
 import "@babylonjs/core/Loading/loadingScreen";
 import "@babylonjs/loaders/glTF";
 import "@babylonjs/core/Animations/animatable";
 import "@babylonjs/core/Materials/Node/Blocks";
-
 /*
  * BabylonJsGraphicsSystem
  *
@@ -62,6 +64,52 @@ export class BabylonJsGraphicsSystem implements System {
     });
 
     this.#engine.getRenderingCanvas()?.removeAttribute("hidden");
+    this.#engine.resize();
+
+    var sphere = MeshBuilder.CreateSphere(
+      "sphere1",
+      { segments: 16, diameter: 2 },
+      this.#scene
+    );
+    sphere.position.y = 2;
+    sphere.position.z = 5;
+
+    //     const assetArrayBuffer = Tools.LoadFileAsync(
+    //       "https://w3s.link/ipfs/QmdPXtkGThsWvR1YKg4QVSR9n8oHMPmpBEnyyV8Tk638o9",
+    //       true
+    //     );
+    //
+    //     assetArrayBuffer
+    //       .then((v) => {
+    //         const assetBlob = new Blob([v]);
+    //         const assetUrl = URL.createObjectURL(assetBlob);
+    //         return SceneLoader.ImportMeshAsync(
+    //           "",
+    //           "./",
+    //           assetUrl,
+    //           this.#scene,
+    //           undefined,
+    //           ".glb"
+    //         );
+    //       })
+    //       .then(({ meshes }) => {
+    //         const mesh = meshes[0];
+    //
+    //         // Set the camera position to center on the mesh
+    //         const boundingBox = mesh.getBoundingInfo().boundingBox;
+    //         const center = boundingBox.center;
+    //         const radius = boundingBox.extendSize.length();
+    //         const camera = new ArcRotateCamera(
+    //           "camera",
+    //           0,
+    //           0,
+    //           radius * 2,
+    //           center,
+    //           scene
+    //         );
+    //         camera.setTarget(center);
+    //         camera.attachControl(canvas, true);
+    //       });
   }
 
   update(
